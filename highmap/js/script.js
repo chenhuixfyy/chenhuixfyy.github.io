5f2866c216276cac071d23ccc6d0ba730d5dd6b7
chenhui <chenhui@haizhi.com>
2016-04-22 14:48:12 +0800

    map

--- highmap/js/script.js
+++ highmap/js/script.js
@@ -47,9 +47,8 @@ $(function () {
                             contentType: "application/json; charset=utf-8",
                             dataType:'jsonp',
                             crossDomain: true,
-                            success: function(json) {
+                            success: function(json) {   
                                 data = Highcharts.geojson(json);
-															 
                                 $.each(data, function (i) {									 
                                     this.value = i;
 									this.events={};
@@ -68,7 +67,8 @@ $(function () {
 									},
                                     dataLabels: {
                                         enabled: true,
-                                        format: '{point.name}'
+                                        format: '{point.name}',
+                                        color: '#fff'
                                     }
                                 });
                             },
@@ -230,14 +230,94 @@ function base64decode(str) {
     out += String.fromCharCode(((c3 & 0x03) << 6) | c4);  
     }  
     return out;  
-}  
-// function goHome(){
-// 	window.open("http://www.peng8.net/");
-// }
-function getGithub()
-	{
-		$.getJSON("https://api.github.com/repos/peng8/GeoMap/contents/json/bei_jing.geo.json", function(data){
-		console.log(base64decode(data.content));
-});
-		
-	}
+}
+function getGithub(){
+	$.getJSON("https://api.github.com/repos/peng8/GeoMap/contents/json/bei_jing.geo.json", function(data){
+	  console.log(base64decode(data.content));
+    });
+}
+// $.getJSON("https://rawgit.com/ecomfe/echarts/master/map/json/province/chongqing.json", function(data){
+//       console.log(decode(data));
+// });
+function decode(json) {
+    if (!json.UTF8Encoding) {
+        return json;
+    }
+    var features = json.features;
+
+    for (var f = 0; f < features.length; f++) {
+        var feature = features[f];
+        var geometry = feature.geometry;
+        var coordinates = geometry.coordinates;
+        var encodeOffsets = geometry.encodeOffsets;
+
+        for (var c = 0; c < coordinates.length; c++) {
+            var coordinate = coordinates[c];
+
+            if (geometry.type === 'Polygon') {
+                coordinates[c] = decodePolygon(
+                    coordinate,
+                    encodeOffsets[c]
+                );
+            }
+            else if (geometry.type === 'MultiPolygon') {
+                for (var c2 = 0; c2 < coordinate.length; c2++) {
+                    var polygon = coordinate[c2];
+                    coordinate[c2] = decodePolygon(
+                        polygon,
+                        encodeOffsets[c][c2]
+                    );
+                }
+            }
+        }
+    }
+    // Has been decoded
+    json.UTF8Encoding = false;
+    return json;
+}
+
+function decodePolygon(coordinate, encodeOffsets) {
+    var result = [];
+    var prevX = encodeOffsets[0];
+    var prevY = encodeOffsets[1];
+
+    for (var i = 0; i < coordinate.length; i += 2) {
+        var x = coordinate.charCodeAt(i) - 64;
+        var y = coordinate.charCodeAt(i + 1) - 64;
+        // ZigZag decoding
+        x = (x >> 1) ^ (-(x & 1));
+        y = (y >> 1) ^ (-(y & 1));
+        // Delta deocding
+        x += prevX;
+        y += prevY;
+
+        prevX = x;
+        prevY = y;
+        // Dequantize
+        result.push([x / 1024, y / 1024]);
+    }
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
