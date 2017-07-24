4d9d3eac7f78c1e62c6ec3f37e20c8ee4ab5c133
chenhui <chenhui@haizhi.com>
2017-07-24 19:19:33 +0800

    呼市房管局

--- hs/js/index.js
+++ hs/js/index.js
@@ -1,12 +1,34 @@
 $(function () {
 
 	// 首页banner
-    var swiper = new Swiper('.banner-container', {
+    var swiperBanner = new Swiper('.banner-container', {
         pagination: '.banner-pagination',
         paginationClickable: true,
         autoplay: 5000,
         loop: true
     });
+	// 内容banner
+    var swiperContent = new Swiper('.content-container', {
+        pagination: '.content-pagination',
+        paginationClickable: true,
+        autoplay: 3000,
+        loop: true
+    });
+
+    // tab
+    var tabsSwiper = new Swiper('.tabs-container',{
+	    onlyExternal : true,
+	    speed:0
+	})
+	$(".tabs a").on('touchstart mousedown',function(e){
+	    e.preventDefault()
+	    $(".tabs .active").removeClass('active')
+	    $(this).addClass('active')
+	    tabsSwiper.swipeTo( $(this).index() )
+	})
+	$(".tabs a").click(function(e){
+	    e.preventDefault()
+	})
 
 
 
